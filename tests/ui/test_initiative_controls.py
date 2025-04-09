5f0660ca8c2d29e1d0bea53302cee4a3841c0e34
Conengmo <33519926+Conengmo@users.noreply.github.com>
2025-04-09 10:16:58 +0200

    use qtbot.waitSignal in test

--- tests/ui/test_initiative_controls.py
+++ tests/ui/test_initiative_controls.py
@@ -1,5 +1,3 @@
-
-
 from battle_map_tv.layouts.initiative_controls import InitiativeTextArea
 from battle_map_tv.utils import find_child_by_attribute
 
@@ -10,11 +8,11 @@ def test_initiative_text_area_updates_image_window(image_window, gui_window, qtb
     assert len(image_window.initiative_overlay_manager.overlays) == 0
 
     # Simulate writing text in the text area
-    qtbot.keyClicks(text_area, "Test Initiative Order")
-    qtbot.wait(text_area.text_changed_debounce_ms * 2)
+    with qtbot.waitSignal(text_area.textChangedDebounced, timeout=5000):
+        qtbot.keyClicks(text_area, "Test Initiative Order")
     assert len(image_window.initiative_overlay_manager.overlays) == 2
 
     # Simulate clearing the text area
-    qtbot.keyClicks(text_area, "\b" * len("Test Initiative Order"))
-    qtbot.wait(text_area.text_changed_debounce_ms * 2)
+    with qtbot.waitSignal(text_area.textChangedDebounced, timeout=5000):
+        qtbot.keyClicks(text_area, "\b" * len("Test Initiative Order"))
     assert len(image_window.initiative_overlay_manager.overlays) == 0
